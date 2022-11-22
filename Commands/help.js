47f81e9bec9b13e1d6509e84ce142baf6c42d5c1
00Shinigami <mail@max-ramacher.de>
2022-11-22 14:22:32 +0100

    expand help

--- Commands/help.js
+++ Commands/help.js
@@ -17,16 +17,63 @@ module.exports = {
           .addFields({
             name: '/info',
             value: 'Zeigt dir noch unbekanntes an.',
+            value: 'Hier steht immernoch nichts',
             inline: true
           },
           {
             name: '/PSSO',
             value: 'Der Weg zum PSSO System,',
+            value: 'https://psso.th-koeln.de/qisserver/rds?state=user&type=0',
             inline: true
           },
           {
             name: '/Kantine',
-            value: 'Hier kannst du dir den Speiseplan angucken.',
+            value: 'Alle nützlichen Informationen über die Kantine findest du hier',
+            value:'https://www.kstw.de/allgemein/standort-gummersbach?searchterm=Gummersbach',
+            inline: true
+          });
+
+          await interaction.reply({
+            embeds : [embed]
+          })
+  },
+  data: new SlashCommandBuilder().setName('kantine').setDescription('Alle nützlichen Informationen über die Kantine findest du hier'),
+  async execute(interaction, client) {
+    const embed = new EmbedBuilder()
+          .setTitle('Kantine')
+          .setDescription('Schau mal nach was es heute zu essen gibt')
+          .setColor(0x18e1ee)
+          //.setImage(client.user.displayAvatarURL())
+          //.setThumbnail(client.user.displayAvatarURL())
+          .setTimestamp(Date.now())
+          //.setFooter({
+          //  iconURL: client.user.displayAvatarURL(),
+          //  text: client.user.tag
+          //})
+          .addFields({
+            value:'https://www.kstw.de/allgemein/standort-gummersbach?searchterm=Gummersbach',
+            inline: true
+          });
+
+          await interaction.reply({
+            embeds : [embed]
+          })
+  },
+  data: new SlashCommandBuilder().setName('psso').setDescription('Hier gibts einen Link zum PSSO System'),
+  async execute(interaction, client) {
+    const embed = new EmbedBuilder()
+          .setTitle('Prüfungs- und Studierendenservice Online')
+          .setDescription('Schau mal nach was es hier gibt..')
+          .setColor(0x18e1ee)
+          //.setImage(client.user.displayAvatarURL())
+          //.setThumbnail(client.user.displayAvatarURL())
+          .setTimestamp(Date.now())
+          //.setFooter({
+          //  iconURL: client.user.displayAvatarURL(),
+          //  text: client.user.tag
+          //})
+          .addFields({
+            value:'https://psso.th-koeln.de/qisserver/rds?state=user&type=0',
             inline: true
           });
 
