e011b84202230e869a5f6ba0623b5212c0729cd8
00Shinigami <mail@max-ramacher.de>
2022-11-27 14:14:16 +0100

    add psso link embed

---
+++ Commands/psso.js
@@ -0,0 +1,21 @@
+const { SlashCommandBuilder, EmbedBuilder, Embed } = require('discord.js');
+
+module.exports = {
+  data: new SlashCommandBuilder().setName('psso').setDescription('Hier gibts einen Link zum PSSO System'),
+  async execute(interaction, client) {
+    const embed = new EmbedBuilder()
+          .setTitle('Prüfungs- und Studierendenservice Online')
+          .setDescription('Schau mal nach was es hier gibt..')
+          .setColor('Random')
+          .setTimestamp(Date.now())
+          .addFields({
+            name: 'Link:',
+            value: 'https://psso.th-koeln.de/qisserver/rds?state=user&type=0',
+            inline: true
+          });
+
+          await interaction.reply({
+            embeds : [embed]
+          })
+  },
+};
