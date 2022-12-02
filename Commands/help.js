4be4d5039224f69758fad6753b5711cf267b2bf2
Nick S <Nick.Struckmeyer@student.hpi.uni-potsdam.de>
2022-12-02 18:46:19 +0100

    Made help-command dynamic

--- Commands/help.js
+++ Commands/help.js
@@ -1,30 +1,26 @@
 const { SlashCommandBuilder, EmbedBuilder, Embed } = require('discord.js');
 
 module.exports = {
-  data: new SlashCommandBuilder().setName('help').setDescription('Öffnet die Hilfe mit allen Befehlen'),
-  async execute(interaction, client) {
+  data: new SlashCommandBuilder()
+    .setName('help')
+    .setDescription('Öffnet die Hilfe mit allen Befehlen'),
+  async execute(interaction) {
+    const { commands } = interaction.client;
+
     const embed = new EmbedBuilder()
       .setTitle('Hilfe')
-      .setDescription('Eine Liste mit allen Befehlen.')
+      .setThumbnail(interaction.client.user.displayAvatarURL())
+      .setDescription('Eine Liste mit allen Befehlen!')
       .setColor('Random')
-      .setTimestamp(Date.now())
-      .addFields(
-        {
-          name: '/info',
-          value: 'Zeigt dir noch unbekanntes an.',
-          inline: true,
-        },
-        {
-          name: '/psso',
-          value: 'Der Weg zum PSSO System,',
-          inline: true,
-        },
-        {
-          name: '/mensa',
-          value: 'Falls du wissen willst, was es morgen zu Essen gibt, gibt /mensa [Datum] ein!',
-          inline: true,
-        }
-      );
+      .setTimestamp(Date.now());
+
+    commands.map((command) => {
+      embed.addFields({
+        name: command.data.name,
+        value: command.data.description,
+        inline: false,
+      });
+    });
 
     await interaction.reply({
       embeds: [embed],
