c0257eccd43058b8765d2b0e8edb8cf2a668c003
Nick S <Nick.Struckmeyer@student.hpi.uni-potsdam.de>
2022-12-02 18:55:35 +0100

    Added Event handling

--- main.js
+++ main.js
@@ -1,35 +1,15 @@
-const { Client, Events, GatewayIntentBits } = require('discord.js');
+const { Client, GatewayIntentBits } = require('discord.js');
 const { token } = require('./config.json');
 const { handleCommands } = require('./Utils/command_handler');
+const { handleEvents } = require('./Utils/event_handler');
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-client.once(Events.ClientReady, (bot) => {
-  console.log(`Ready! Logged in as ${bot.user.tag}`);
-});
-
 client.login(token);
-
-client.on(Events.InteractionCreate, async (interaction) => {
-  if (!interaction.isChatInputCommand()) return;
-
-  const command = interaction.client.commands.get(interaction.commandName);
-
-  if (!command) {
-    console.error(`No command matching ${interaction.commandName} was found.`);
-    return;
-  }
-
-  try {
-    await command.execute(interaction);
-  } catch (error) {
-    console.error(error);
-    await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-  }
-});
 
 async function main() {
   await handleCommands(client);
+  await handleEvents(client);
 }
 
 main();
