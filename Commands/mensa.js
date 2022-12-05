b37d6c135f8d4be5bbdf98aec4bf201c8498c612
Nick S <Nick.Struckmeyer@student.hpi.uni-potsdam.de>
2022-12-05 13:52:11 +0100

    Refactored validateDate function

--- Commands/mensa.js
+++ Commands/mensa.js
@@ -5,16 +5,23 @@ const cheerio = require('cheerio');
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('mensa')
-    .setDescription('Zeigt dir die Gerichte der Mensa zu einem bestimmten Datum an!')
+    .setDescription(
+      'Zeigt dir die Gerichte der Mensa zu einem bestimmten Datum an!'
+    )
     .addStringOption((option) =>
-      option.setName('datum').setDescription('Das Datum zu welchem Gerichte gesucht werden sollen z.B. 2022-11-28')
+      option
+        .setName('datum')
+        .setDescription(
+          'Das Datum zu welchem Gerichte gesucht werden sollen z.B. 2022-11-28'
+        )
     ),
   async execute(interaction) {
     fetchMensaData();
     await interaction.deferReply();
 
     const specifiedDate =
-      interaction.options.get('datum')?.value.trim().toLowerCase() || new Date().toJSON().slice(0, 10);
+      interaction.options.get('datum')?.value.trim().toLowerCase() ||
+      new Date().toJSON().slice(0, 10);
 
     const dateIsValid = validateDate(specifiedDate);
     const convertedDate = convertDate(specifiedDate);
@@ -27,14 +34,18 @@ module.exports = {
     const dishes = await fetchMensaData(convertedDate);
 
     if (Object.keys(dishes).length === 0)
-      return interaction.editReply(`Für das Datum ${convertedDate} wurden leider keine Gerichte gefunden!`);
+      return interaction.editReply(
+        `Für das Datum ${convertedDate} wurden leider keine Gerichte gefunden!`
+      );
 
     const embed = new EmbedBuilder()
       .setTitle('Mensa-Gerichte')
       .setDescription(`Gerichte in der Mensa am ${convertedDate}`)
       .setColor('#ffd600')
       .setTimestamp(Date.now())
-      .setThumbnail('https://www.kstw.de/typo3conf/ext/ep_template_werk/Resources/Public/Icons/icon_koeln.png');
+      .setThumbnail(
+        'https://www.kstw.de/typo3conf/ext/ep_template_werk/Resources/Public/Icons/icon_koeln.png'
+      );
 
     for (const dish in dishes) {
       embed.addFields({
@@ -57,22 +68,24 @@ async function fetchMensaData(date) {
   const mensaData = await response.text();
 
   const mensaDOM = cheerio.load(mensaData);
-  mensaDOM('[data-location="22"] .col-8 .tx-epwerkmenu-menu-meal-title').each((index, element) => {
-    const description = mensaDOM('[data-location="22"] .col-8 .tx-epwerkmenu-menu-meal-description')[
-      index
-    ].children[0].data.trim();
+  mensaDOM('[data-location="22"] .col-8 .tx-epwerkmenu-menu-meal-title').each(
+    (index, element) => {
+      const description = mensaDOM(
+        '[data-location="22"] .col-8 .tx-epwerkmenu-menu-meal-description'
+      )[index].children[0].data.trim();
 
-    const price = mensaDOM('[data-location="22"] .col-8 .text')
-      [index].children[2].data.match(/([0-9]{1}\,[0-9]{2})/g)
-      .map((price) => `${price}€`)
-      .join(' | ');
+      const price = mensaDOM('[data-location="22"] .col-8 .text')
+        [index].children[2].data.match(/([0-9]{1}\,[0-9]{2})/g)
+        .map((price) => `${price}€`)
+        .join(' | ');
 
-    dishes[element.children[0].data.trim()] = {
-      name: element.children[0].data.trim(),
-      description,
-      price,
-    };
-  });
+      dishes[element.children[0].data.trim()] = {
+        name: element.children[0].data.trim(),
+        description,
+        price,
+      };
+    }
+  );
 
   return dishes;
 }
@@ -82,9 +95,7 @@ function validateDate(date) {
 
   const dateRegex = new RegExp('(?:^[0-9]{4})-(?:[0-9]{2})-(?:[0-9]{2}$)');
 
-  if (dateRegex.test(date)) return true;
-
-  return false;
+  return dateRegex.test(date);
 }
 
 function convertDate(date) {
@@ -92,6 +103,8 @@ function convertDate(date) {
 
   let newDate = new Date();
 
-  date === 'morgen' ? newDate.setDate(newDate.getDate() + 1) : newDate.setDate(newDate.getDate() + 2);
+  date === 'morgen'
+    ? newDate.setDate(newDate.getDate() + 1)
+    : newDate.setDate(newDate.getDate() + 2);
   return newDate.toJSON().slice(0, 10);
 }
