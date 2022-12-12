b3c39cf38dcc77b3bc629a04696c0a9ff83f0581
Cool_1001 <66476592+Eintausendeins@users.noreply.github.com>
2022-12-12 21:44:19 +0100

    quick fix to avoid mistakes when assigning roles on join

--- Events/autorole_join.js
+++ Events/autorole_join.js
@@ -1,9 +1,12 @@
 const { Events } = require('discord.js');
+const { guildId } = require('../config.json');
 
 module.exports = {
   name: Events.GuildMemberAdd,
   once: false,
   async execute(member) {
+
+    if(member.guild.id !== guildId) return;
 
     const role = member.guild.roles.cache.get("1019231681024098304");
     await member.roles.add(role.id);
