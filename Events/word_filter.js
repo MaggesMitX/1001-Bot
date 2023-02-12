c37aff14a5e3b3f69bc147d03c01e1764be2ae2b
Maximilian Ramacher <mail@max-ramacher.de>
2023-02-12 19:49:12 +0100

    line 9: add message delete phrase

--- Events/word_filter.js
+++ Events/word_filter.js
@@ -6,8 +6,8 @@ module.exports = {
   async execute(message) {
     for (const word of message.content.split(' ')) {
       if (message.client.bannedWords[word.toLowerCase()]) {
-        await message.delete();
         const msgToDelete = await message.reply("Deine Nachricht enthielt unangemessene Sprache und wurde gelöscht.");
+        await message.delete();
         setTimeout(async () => {
           await msgToDelete.delete();
         }, 3000);
