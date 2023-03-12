82e5d8bd6b4884b7a3f96d0a5a233ab1a41ee3c0
Maximilian Ramacher <mail@max-ramacher.de>
2023-03-12 20:55:37 +0100

    ignor bot messages

--- Events/word_filter.js
+++ Events/word_filter.js
@@ -4,6 +4,9 @@ module.exports = {
   name: Events.MessageCreate,
   once: false,
   async execute(message) {
+    if (message.author.bot){
+      return;
+    }
     for (const word of message.content.split(' ')) {
       if (message.client.bannedWords[word.toLowerCase()]) {
         const msgToDelete = await message.reply("Deine Nachricht enthielt unangemessene Sprache und wurde gelöscht.");
