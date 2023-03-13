80719968dc719d02f040a2157eabc2886fe089b3
Maximilian Ramacher <mail@max-ramacher.de>
2023-03-13 01:25:22 +0100

    finish command

---
+++ Commands/2048.js
@@ -0,0 +1,38 @@
+const { SlashCommandBuilder} = require('discord.js');
+const { TwoZeroFourEight } = require('discord-gamecord');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('2048')
+        .setDescription('Spiele 2048')
+        .addUserOption((option) =>
+            option
+                .setName('opponent')
+                .setDescription('Gegner')
+        ),
+    async execute(interaction) {
+        const Game = new TwoZeroFourEight({
+            message: interaction,
+            isSlashGame: false,
+            embed: {
+                title: '2048',
+                color: '#5865F2'
+            },
+            emojis: {
+                up: '⬆️',
+                down: '⬇️',
+                left: '⬅️',
+                right: '➡️',
+            },
+            timeoutTime: 60000,
+            buttonStyle: 'PRIMARY',
+            playerOnlyMessage: 'Nur {player} kann diese Schaltfläche verwenden.'
+        });
+
+        await Game.startGame();
+        await Game.on('gameOver', result => {
+            interaction.editReply('Verloren')
+            console.log(result);  // =>  { result... }
+        });
+    }
+}
