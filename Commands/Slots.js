2250584d7025edbaeb392b12092c738fb4baf740
Maximilian Ramacher <mail@max-ramacher.de>
2023-03-13 01:33:49 +0100

    finished

---
+++ Commands/Slots.js
@@ -0,0 +1,24 @@
+const { SlashCommandBuilder} = require('discord.js');
+const { Slots } = require('discord-gamecord');
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('slots')
+        .setDescription('Spiele Slots'),
+    async execute(interaction){
+        const Game = new Slots({
+            message: interaction,
+            isSlashGame: false,
+            embed: {
+                title: 'Slot Machine',
+                color: '#5865F2'
+            },
+            slots: ['🍇', '🍊', '🍋', '🍌']
+        });
+
+        await Game.startGame();
+         await Game.on('gameOver', result => {
+             interaction.editReply('Verloren')
+             console.log(result);  // =>  { result... }
+        });
+    }
+}
