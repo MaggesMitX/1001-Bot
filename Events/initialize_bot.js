36638aa6f24ffe983cd6e3045496067a489118bb
Maximilian Ramacher <mail@max-ramacher.de>
2023-03-13 02:41:16 +0100

    add new list

--- Events/initialize_bot.js
+++ Events/initialize_bot.js
@@ -1,12 +1,15 @@
 const { Events } = require('discord.js');
 const bannedWords = require('../banned_words.json').words;
+const customWords = require('../custom_words.json').words;
 
 module.exports = {
   name: Events.ClientReady,
   once: true,
   execute(client) {
+    client.customWords = [];
     client.bannedWords = [];
     client.rateLimiter = new Map();
     bannedWords.map((word) => (client.bannedWords[word] = true));
+    customWords.map((word) => (client.customWords[word] = true));
   },
 };
