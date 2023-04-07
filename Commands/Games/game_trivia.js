93aba869f143c1cd1c89914dede894df18cf47a7
Cool_1001 <66476592+Eintausendeins@users.noreply.github.com>
2023-04-08 00:29:23 +0200

    added difficulty selection to trivia

--- Commands/Games/game_trivia.js
+++ Commands/Games/game_trivia.js
@@ -2,9 +2,27 @@ import { SlashCommandBuilder } from 'discord.js';
 import { Trivia } from 'discord-gamecord';
 import { handleGameEnd } from "../../Utils/money.js";
 
+const choices = [
+  {name: "Easy", value: "easy"},
+  {name: "Medium", value: "medium"},
+  {name: "Hard", value: "hard"}
+];
+
+
 export default {
-  data: new SlashCommandBuilder().setName('trivia').setDescription('Spiele Trivia').setDMPermission(false),
+  data: new SlashCommandBuilder().setName('trivia').setDescription('Spiele Trivia').setDMPermission(false)
+      .addStringOption((option) =>
+          option
+              .setName('difficulty')
+              .setNameLocalizations({de: 'schwierigkeit'})
+              .setDescription('Choose the difficulty of the question')
+              .setDescriptionLocalizations({de: 'Wähle die Schwierigkeit der Frage'})
+              .setChoices(...choices)),
   async execute(interaction) {
+
+    let difficultyInput = interaction.options.get('difficulty')?.value;
+    if(!difficultyInput) difficultyInput = "medium";
+
     const Game = new Trivia({
       message: interaction,
       isSlashGame: true,
@@ -18,7 +36,7 @@ export default {
       trueButtonStyle: 'SUCCESS',
       falseButtonStyle: 'DANGER',
       mode: 'multiple', // multiple || single
-      difficulty: 'medium', // easy || medium || hard
+      difficulty: difficultyInput, // easy || medium || hard
       winMessage: 'Gewonnen! Die richtige Antwort ist {answer}.',
       loseMessage: 'Leider verloren! Die richtige Antwort wäre {answer}.',
       errMessage: 'Es können keine Fragedaten abgerufen werden! Bitte versuche es erneut.',
@@ -28,7 +46,15 @@ export default {
     await Game.startGame();
     await Game.on('gameOver', (result) => {
       //console.log(result);  // =>  { result... }
-      handleGameEnd(interaction, result, "Trivia", 2, 1);
+
+      const difficulty = result.question.difficulty;
+      const coins = {
+        hard: [10, 15],
+        medium: [5, 5],
+        default: [3, 3],
+      }[difficulty] || [3, 3];
+
+      handleGameEnd(interaction, result, "Trivia", ...coins);
     });
   },
 };
